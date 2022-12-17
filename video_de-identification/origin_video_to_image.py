2246adfc29e8b4b6a3d3a37575c53df6a98288a2
dczqcc <112818068+dczqcc@users.noreply.github.com>
2022-12-18 06:03:33 +0900

    Create origin_video_to_image.py
    make image sequence from origin video

---
+++ video_de-identification/origin_video_to_image.py
@@ -0,0 +1,29 @@
+import cv2
+ 
+def main():
+    capture = cv2.VideoCapture('Desktop/test/origin.mp4')
+    num=0
+    if (capture.isOpened()== False): 
+        print("Error opening video stream or file")
+    
+    while(capture.isOpened()):
+        ret, frame = capture.read()
+      
+        if ret == True:
+            cv2.imshow('Frame', frame)
+            path='Desktop/test/origin/origin_' + str(num) + '.jpg'
+            cv2.imwrite(path,frame)
+          
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+        else:
+            break
+        num += 1
+     
+    capture.release()
+     
+    cv2.destroyAllWindows()
+ 
+ 
+if __name__ == '__main__':
+    main()
